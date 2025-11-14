7ab1ff38614f161e7cdb6654b0b67ec3d937faea
muhstefan <muhstefang@gmail.com>
2025-11-14 12:22:01 +0300

    The task model has been edited to include an additional status column

--- src/main/kotlin/com/example/TaskApp/model/Task.kt
+++ src/main/kotlin/com/example/TaskApp/model/Task.kt
@@ -2,12 +2,19 @@ package com.example.TaskApp.model
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.time.LocalDate
 import java.util.UUID
+
+enum class TaskStatus {
+    WAITING,
+    COMPLETE
+}
 
 @Table(name = "tasks")
 @Entity
@@ -26,6 +33,10 @@ open class Task {
 
     @Column(name = "due_date", nullable = false) 
     lateinit var dueDate: LocalDate  
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    open var status: TaskStatus = TaskStatus.WAITING
 
 
 }
